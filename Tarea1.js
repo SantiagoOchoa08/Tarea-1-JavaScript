8dea94057bd9475c61ea300c390d0baabde2582a
SantiagoOchoa08 <santiagoochoaherrera08@gmail.com>
2024-09-11 23:11:49 -0500

    Tarea completada

---
+++ Tarea1.js
@@ -0,0 +1,56 @@
+// Definir 10 variables let
+
+let numero1 = 8;
+let numero2 = 2;
+let numero3 = 25;
+let dialogo1 = "Hola, te encuentras bien el día de hoy?";
+let dialogo2 = "Holaaa, sí, me encuentro bastante bien hoy, es un buen día";
+let figura = "triangulo";
+let color = "Rojo";
+let color2 = "Azul";
+let color3 = "Purpura";
+let CantAmigos = 3;
+let edad = 19;
+
+// Definir 5 variables const
+
+const euler = 2.7182; 
+const gravedad = 9.8;
+const llantas = 4;
+const despedida = "Adíos, nos vemos luego";
+const despedida2 = "Así será, mucha suerte";
+
+// Operaciones con las variables
+
+let saludo = " Un dia normal dos amigos se encuentran " + dialogo1 + dialogo2;
+
+console.log(saludo);
+
+let suma = numero1 + numero2;
+let resta = numero3 - numero1;
+let multiplicación = numero3 * numero2;
+let division = numero1 / numero2;
+
+console.log(suma);
+console.log(resta);
+console.log(multiplicación);
+console.log(division);
+
+let mezcla = "Juntando " + color + " Y " + color2 + " Obtenemos " + color3;
+console.log(mezcla);
+
+// Operador ternario
+
+let MayorEdad = edad >= 18 ? "Es mayor de edad" : "Es menor de edad";
+console.log(MayorEdad);
+
+let FiguraCorrecta = figura === "Circulo" ? true : false;
+console.log(FiguraCorrecta);
+  
+ 
+
+
+
+
+
+
